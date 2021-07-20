de07de5d1357db5d2ffb320deb412e5f15899cad
Yoaki Niscoise <yoakiniscoise@gmail.com>
2021-07-20 21:11:10 +0200

    Footer ul li

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -4,8 +4,49 @@ import * as footerStyle from '../styles/footer.module.scss'
 const Footer = () => {
   return (
     <div className={footerStyle.footerzone}>
+      <div className={footerStyle.column}>
+        <h1>Company</h1>
+        <ul>
+          <li>About Us</li>
+          <li>Carreer</li>
+          <li>Inquieres</li>
+          <li>Inquieres - Supplier</li>
+        </ul>
+      </div>
 
+      <div className={footerStyle.column}>
+        <h1>Account</h1>
+        <ul>
+          <li>About</li>
+          <li>Mission</li>
+          <li>Service</li>
+          <li>Social</li>
+          <li>Get in touch</li>
+        </ul>
+      </div>
+
+
+      <div className={footerStyle.column}>
+        <h1>Support</h1>
+        <ul>
+          <li>FAQ</li>
+          <li>Contact us</li>
+          <li>Web Chat</li>
+          <li>Open ticket</li>
+        </ul>
+      </div>
+
+      <div className={footerStyle.column}>
+        <h1>Social Media</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
     </div>
+
+
   )
 }
 
--- src/pages/components/PlanZone.js
+++ src/pages/components/PlanZone.js
@@ -5,20 +5,11 @@ const PlanZone = () => {
   return (
     <div className={planStyle.choosezone}>
       <div className={planStyle.container}>
-        <div className={planStyle.title}>
-          Choose a Plan
-          <p>See why over 1 million members love us! </p>
-        </div>
-        <div className={planStyle.card}>
-          <h1>KuramIA</h1>
-        </div>
-        <div className={planStyle.card}>
-          <h1>Classic</h1>
-        </div>
         <div className={planStyle.lowzone}>
           <div className={planStyle.text}>Ready.Set.Go</div>
-          <input className={planStyle.input}></input>
-          <button className={planStyle.button} placeholder="Email" >Invest Now</button>
+          <p className={planStyle.subtext}>See why over 1 million members love us! </p>
+          <input className={planStyle.input} placeholder="Email"></input>
+          <button className={planStyle.button}  >Invest Now</button>
         </div>
       </div>
     </div>
